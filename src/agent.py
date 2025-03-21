11d96565d9c4cd2a39b9d9445ea30b0474a5d288
motacapla <t.i.motacapla@gmail.com>
2025-03-22 08:19:55 +0900

    update prompt and checkpointer for memory

--- src/agent.py
+++ src/agent.py
@@ -1,14 +1,16 @@
-import os
+from uuid import uuid4
+
 from langchain_ollama import OllamaLLM
 
 # from langchain.agents import AgentExecutor, create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.tools import Tool
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
-from langchain.prompts import PromptTemplate
-from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain_core.tools import Tool
 from langgraph.prebuilt import create_react_agent
+from langchain.prompts import ChatPromptTemplate
 
 from tools import current_datetime
 
@@ -104,19 +106,26 @@ tools = [
 
 tool_names = [tool.name for tool in tools] + usable_default_tool_names
 
-prompt = f"""あなたは優秀なAIアシスタントです。与えられたタスクを解決してください。必要に応じて提供されているツールを使用してください。
-日本語で回答してください。
-利用可能なツール: {tool_names}
-"""
-
-system_prompt = prompt
-agent_executor = create_react_agent(model=model, tools=tools, prompt=system_prompt)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "あなたは優秀なAIアシスタントです。与えられたタスクを解決してください。必要に応じて提供されているツールを使用してください。日本語で回答してください。",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
+memory = MemorySaver()
+agent_executor = create_react_agent(
+    model=model, tools=tools, prompt=prompt, checkpointer=memory
+)
+config = {"configurable": {"thread_id": str(uuid4())}}
 
 if __name__ == "__main__":
     user_input = input("Task: ")
     inputs = {"messages": [("user", user_input)]}
 
-    for s in agent_executor.stream(inputs, stream_mode="values"):
+    for s in agent_executor.stream(inputs, stream_mode="values", config=config):
         message = s["messages"][-1]
         if isinstance(message, tuple):
             print(message)
